b5e9b65a40638cfcaacb948873e89f22c6fa9db1
Miyaki <144659450+sendjiMiyaki@users.noreply.github.com>
2024-11-13 17:05:27 +0000

    TP

--- code/exercice 0/statique.php
+++ code/exercice 0/statique.php
@@ -12,17 +12,18 @@
     moyenne(array("francais"=>15, "maths"=>5, "engene"=>20, "anglais"=>19));
 
     // médiane
-    function mediane($donnees) {
+    function mediane(array $donnees) {
         foreach($donnees as $valeur) {
-            if (sizeof(donnee)%2 == 0) {
-                $milieu = donnee[(sizeof(donnee)-1 * sizeof(donnee)+1) / 2];
+            if (sizeof($donnees)%2 == 0) {
+                $milieu = $donnees[(sizeof($donnees)-1 + sizeof($donnees)+1) / 2];
             }
             else {
-                $milieu = donnee[sizeof(donne/2)];
+                $milieu = $donnees[sizeof($donnees/2)];
             }
         }
         echo "la médiane est ". $milieu ."\n";
     }
-    mediane(2004, 2001, 2002, 2002, 2002, 2003, 2005); // yang jungwon, lee heeseung, park jonseong, sim jaeyun, park sunghoon, kim sunoo, nishimura riki
-    mediane(2004, 2001, 2002, 2002, 2002, 2003, 2005, 2020); // yang jungwon, lee heeseung, park jonseong, sim jaeyun, park sunghoon, kim sunoo, nishimura riki, en-
+    
+    mediane(array(2004, 2001, 2002, 2002, 2002, 2003, 2005)); // yang jungwon, lee heeseung, park jonseong, sim jaeyun, park sunghoon, kim sunoo, nishimura riki
+    mediane(array(2004, 2001, 2002, 2002, 2002, 2003, 2005, 2020)); // yang jungwon, lee heeseung, park jonseong, sim jaeyun, park sunghoon, kim sunoo, nishimura riki, en-
 ?>
---
+++ code/formulaire/assets/js/myscript.js
@@ -0,0 +1,58 @@
+let prenom = document.querySelector('[placeholder="Name"]');  // Name field
+let nom = document.querySelector('[placeholder="First name"]');  // First name field
+let email = document.querySelector('[placeholder="Email"]');  // Email field
+let mdp = document.querySelector('[placeholder="password"]');  // Password field
+let message = document.querySelector('[placeholder="Message"]');  // Message field
+
+/*email et mdp conditions*/
+let maj = /[A-Z]/; //alphabet en majuscule
+let special = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/; //tous les caractères spéciaux
+
+/*selectionner la case à cocher et le texte à côté*/
+let cocher = document.getElementById('formCheck-1');
+let age = document.querySelector('label[for="formCheck-1"]');
+
+/*afficher le message invisible pour le mdp*/
+let messageErreur = document.querySelector(".text-danger");
+
+function verifierFormulaire() {
+    if (prenom.value === "") {
+      prenom.style.borderColor = "red";
+    } else {
+      prenom.style.borderColor = "green";
+    }
+
+    if (nom.value === "") {
+      nom.style.borderColor = "red";
+    } else {
+      nom.style.borderColor = "green";
+    }
+    
+    /*condition : si email.value contient "@"*/
+    if (email.value === "" || !email.value.includes("@")) {
+      email.style.borderColor = "red";
+    } else if (email.value.includes("@")) {
+      email.style.borderColor = "green";
+    }
+
+    /*condition : mdp > 8 caractères, une majuscule et un caractère spécial sinon affichage de "text-danger invisible"*/
+    if (mdp.value === "" || mdp.value.length < 8 || !mdp.value.match(special) || !mdp.value.match(maj)) {
+      mdp.style.borderColor = "red";
+      messageErreur.classList.remove("invisible");
+    } else if (mdp.value.length >= 8 && mdp.value.match(special) && mdp.value.match(maj)) {
+      mdp.style.borderColor = "green";
+      messageErreur.classList.add("invisible");
+    }
+
+    if (message.value === "") {
+      message.style.borderColor = "red";
+    } else {
+      message.style.borderColor = "green";
+    }
+
+    if (!cocher.checked) {
+      age.style.color = "red";
+    } else if (cocher.checked) {
+      age.style.color = "green";
+    }
+}
