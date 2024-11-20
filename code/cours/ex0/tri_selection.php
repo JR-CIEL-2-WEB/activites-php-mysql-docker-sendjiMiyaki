1f180f42e9c928e945ec5a8249cbb5f20e49879e
Miyaki <144659450+sendjiMiyaki@users.noreply.github.com>
2024-11-20 13:36:40 +0000

    séparation du cours et du TP

---
+++ code/cours/ex0/tri_selection.php
@@ -0,0 +1,15 @@
+<?php
+    // passage par valeur
+    function tri_selection(array $tableau) {
+
+    }
+    
+    // passage par référence
+    function tri_selection(&$tableau) {
+
+    }
+
+    function read_tab($t) {
+        
+    }
+?>
